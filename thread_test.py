f23537892f3221faa2da6d3e403e94cd9c1cff00
mrabedini <abed@shipup.net>
2020-03-13 15:58:34 +0330

    [thread_test]: * Use ThreadPoolExecutor for running do_something.

--- thread_test.py
+++ thread_test.py
@@ -1,29 +1,31 @@
+import concurrent.futures
 import logging
 from logging import StreamHandler
-import threading
 import time
 import timeit
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
 logger = logging.getLogger(__name__)
 
 
 def do_something(wait_time):
     logger.info("Waiting for %d seconds.", wait_time)
     time.sleep(wait_time)
-    logger.info("Wait was done for %d seconds.", wait_time)
+    return f"Wait was done for {wait_time} seconds."
 
-
-t1 = threading.Thread(target=do_something,kwargs={"wait_time":1})
-t2 = threading.Thread(target=do_something,kwargs={"wait_time":2})
 
 start = timeit.default_timer()
-t1.start()
-t2.start()
-t1.join()
-t2.join()
-end = timeit.default_timer()
 
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executer:
+    secs = [5, 4, 3, 2, 1]
+    results = [executer.submit(do_something, sec) for sec in secs]
+    for f in concurrent.futures.as_completed(results):
+        logger.info("result %s", f.result())
+
+end = timeit.default_timer()
 logger.info("Execution took %f seconds", end - start)
 

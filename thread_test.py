d40715def6c9f2acc7c5eb83928efe3973c18dc0
mrabedini <abed@shipup.net>
2020-03-13 14:54:36 +0330

    [thread_test]: * Do threading with arguments.

--- thread_test.py
+++ thread_test.py
@@ -9,24 +9,20 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def do_something():
-    logger.info("Waiting for 1 seconds.")
-    time.sleep(1)
-    logger.info("Wait was done")
+def do_something(wait_time):
+    logger.info("Waiting for %d seconds.", wait_time)
+    time.sleep(wait_time)
+    logger.info("Wait was done for %d seconds.", wait_time)
 
 
-threads = list()
-
-for _ in range(10):
-    t = threading.Thread(target=do_something)
-    threads.append(t)
-
+t1 = threading.Thread(target=do_something,args=[1])
+t2 = threading.Thread(target=do_something,args=[2])
 
 start = timeit.default_timer()
-for thread in threads:
-    thread.start()
-for thread in threads:
-    thread.join()
+t1.start()
+t2.start()
+t1.join()
+t2.join()
 end = timeit.default_timer()
 
 logger.info("Execution took %f seconds", end - start)
